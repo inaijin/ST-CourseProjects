8dbe6e05ee8f7c6b2b7ddd3319c34556e17d918b
inaijin <kourosh@Kouroshs-MacBook-Pro.local>
2024-10-17 20:23:44 +0330

    add RatingTest UnitTest

---
+++ CA1-UnitTesting/Mizdooni/src/test/java/model/RatingTest.java
@@ -0,0 +1,29 @@
+package model;
+
+import mizdooni.model.Rating;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RatingTest {
+    @ParameterizedTest
+    @CsvSource({
+            "3.7, 4",   // Rounds up
+            "4.4, 4",   // Rounds down
+            "4.5, 5",   // Borderline rounds up
+            "5.0, 5",   // Exactly 5
+            "6.2, 5",   // Higher than 5 should cap at 5
+            "0.5, 1",   // Low value rounds up to 1
+            "4.9, 5",   // Close to 5 rounds up to 5
+    })
+    @DisplayName("Test Get Star Count")
+    void testGetStarCount(double overall, int expectedStarCount) {
+        Rating rating = new Rating();
+        rating.overall = overall;
+
+        int starCount = rating.getStarCount();
+        assertEquals(expectedStarCount, starCount, "The star count should match the expected value.");
+    }
+}
