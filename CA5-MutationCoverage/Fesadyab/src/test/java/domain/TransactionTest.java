1b168df078b6b1118a9497e53f0a005b9dcac218
inaijin <kouroshsajjadi@yahoo.com>
2024-12-02 15:48:47 +0330

    test WorkFlow With Incorrect Code

--- CA5-MutationCoverage/Fesadyab/src/test/java/domain/TransactionTest.java
+++ CA5-MutationCoverage/Fesadyab/src/test/java/domain/TransactionTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TransactionTest {
-    int ACCOUNT_ID = 110;
+    int ACCOUNT_ID = 120;
 
     private Transaction txn1;
 
