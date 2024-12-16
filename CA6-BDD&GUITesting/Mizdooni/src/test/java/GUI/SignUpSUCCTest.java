6c37e5567c9ee2daef55aa1899e0831d42a17264
inaijin <kouroshsajjadi@yahoo.com>
2024-12-16 21:12:50 +0330

    added SignUP-SUC GUI Test

---
+++ CA6-BDD&GUITesting/Mizdooni/src/test/java/GUI/SignUpSUCCTest.java
@@ -0,0 +1,88 @@
+package GUI;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class SignUpSUCCTest {
+    private WebDriver driver;
+    private boolean acceptNextAlert = true;
+    private final StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testSignUpSUCC() throws Exception {
+        driver.get("http://localhost:8080/api/swagger-ui/index.html#/");
+        driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div/button")).click();
+        driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div[2]/div/div/div/div[2]/button")).click();
+        driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div[2]/div/div/div[3]/div[2]/div/div/div/textarea")).click();
+        driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div[2]/div/div/div[3]/div[2]/div/div/div/textarea")).clear();
+        driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div[2]/div/div/div[3]/div[2]/div/div/div/textarea")).sendKeys("{\n    \"username\": \"swt\",\n    \"password\": \"fall03\",\n    \"email\": \"swt@gmail.com\",\n    \"address\": {\n        \"country\": \"Iran\",\n        \"city\": \"Tehran\"\n    },\n    \"role\": \"client\"\n}");
+        driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div[2]/div/div/div[3]/div[2]")).click();
+        driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div[2]/div/div[2]/button")).click();
+        driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div[2]/div/div[3]/div[2]/div/div/table/tbody/tr/td")).click();
+        try {
+            assertEquals("200", driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div[2]/div/div[3]/div[2]/div/div/table/tbody/tr/td")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e);
+        }
+        driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div[2]/div/div[2]/button[2]")).click();
+        driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div[2]/div/div/div/div[2]/button[2]")).click();
+        driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div[2]/div/div/div/div[2]/button")).click();
+        driver.findElement(By.xpath("//div[@id='operations-authentication-controller-signup']/div/button")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
